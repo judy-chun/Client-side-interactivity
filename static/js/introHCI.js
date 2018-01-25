2ddef35ff587b57eec7f6978afad3abf83ad5b69
Judy Najin Chun <judynajinchun@Judys-MacBook-Air-3.local>
2018-01-25 11:09:07 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,8 +10,28 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Ask your own heart.");
+		$("#testjs").text("Please Wait...");
+		$("#testjs").toggleClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
+
+function projectClick(e) {
+	//prevent page from reloading
+	e.preventDefault();
+	//In an event handler, $(this) refers
+	//the object that triggered the even
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if(description.length == 0){
+		$(containingProject).append("<div class= 'project-description'><p>Description of the Project.</p></div>");
+	}
+
+	else{
+		description.fadeOut();
+	}
+}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
