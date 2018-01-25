ab2fbc0950910c2c79f1ccb3d093ec99b45c1bc4
Judy Najin Chun <judynajinchun@Judys-MacBook-Air-3.local>
2018-01-25 12:08:31 -0800

    Javascript added

--- hello/app.js
+++ hello/app.js
@@ -8,9 +8,11 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
-var hello = require('./routes/hello');
-
 var app = express();
+var hello = require('./routes/hello');
+app.get('/hello/:userName', hello.view);
+
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
